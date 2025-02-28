189fac8c3f536accacac22a15ada66ae15483a9e
GokulSF4339 <gokul.ganesan@syncfusion.com>
2025-02-28 18:43:24 +0530

    Added tooltip customization sample

--- README.md
+++ README.md
@@ -1,37 +1,3 @@
 # show-tooltip-for-appointment-schedule-wpf
-How to show tool tip for appointment in WPF Schedule (SfScheduler)
 
-KB article - [show-tooltip-for-appointment-schedule-wpf](https://www.syncfusion.com/kb/12235/how-to-show-tooltip-for-appointment-in-wpf-scheduler-calendar)
-
-## Requirements to run the sample
-
-* [Visual Studio](https://visualstudio.microsoft.com/downloads/)
-* .NET Framework 3.5/4.0/4.5/4.5.1/4.6
-* .NET Core 3.1
-* .NET 5.0
-
-Refer to the following link for more details - [System Requirements](https://help.syncfusion.com/wpf/system-requirements)
-
-## How to run the sample
-
-1. Clone the sample and open it in Visual Studio.
-
-   *Note: If you download the sample using the "Download ZIP" option, right-click it, select Properties, and then select Unblock.*
-   
-2. Register your license key in the App.cs file as demonstrated in the following code.
-
-		public App()
-		{
-			//Register Syncfusion license
-			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
-		}
-		
-	Refer to this [link](https://help.syncfusion.com/wpf/licensing/overview) for more details.
-	
-3. Clean and build the application.
-
-4. Run the application.
-
-## License
-
-Syncfusion has no liability for any damage or consequence that may arise by using or viewing the samples. The samples are for demonstrative purposes, and if you choose to use or access the samples, you agree to not hold Syncfusion liable, in any form, for any damage that is related to use, for accessing, or viewing the samples. By accessing, viewing, or seeing the samples, you acknowledge and agree Syncfusion’s samples will not allow you seek injunctive relief in any form for any claim related to the sample. If you do not agree to this, do not view, access, utilize, or otherwise do anything with Syncfusion’s samples.
+This repository contains sample code that demonstrates how to enable the tooltip in WinUI Scheduler control. An interactive tooltip provides additional details about the appointments when the user hovers the mouse over them.
--- SchedulerWPF/SchedulerWPF/ViewModel/SchedulerViewModel.cs
+++ SchedulerWPF/SchedulerWPF/ViewModel/SchedulerViewModel.cs
@@ -1,27 +1,222 @@
-﻿using Syncfusion.UI.Xaml.Scheduler;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media;
-
-namespace WpfScheduler
+﻿namespace WpfScheduler
 {
+    using System;
+    using System.Windows.Media;
+    using System.ComponentModel;
+    using Syncfusion.UI.Xaml.Scheduler;
+    using System.Collections.ObjectModel;
+
     public class SchedulerViewModel
     {
-        public ScheduleAppointmentCollection AppointmentCollection { get; set; } = new ScheduleAppointmentCollection();
+        #region Fields
+
+        /// <summary>
+        /// The schedule appointment collection.
+        /// </summary>
+        private ScheduleAppointmentCollection appointments;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SchdulerViewModel" /> class.
+        /// </summary>
         public SchedulerViewModel()
         {
-            ScheduleAppointment appointment1 = new ScheduleAppointment()
-            {
-                StartTime = new DateTime(2020, 12, 30, 10, 0, 0),
-                EndTime = new DateTime(2020, 12, 30, 11, 0, 0),
-                Subject = "Booked",
-                Location = "Hutchison road",
-                AppointmentBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#889e81"))
-        };
-            AppointmentCollection.Add(appointment1);
+            this.Appointments = this.GenerateRandomAppointments();
         }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the collection of appointments for the scheduler.
+        /// </summary>
+        public ScheduleAppointmentCollection Appointments
+        {
+            get
+            {
+                return appointments;
+            }
+            set
+            {
+                appointments = value;
+                this.RaisePropertyChanged("Appointments");
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Method to generate scheduler appointments based on current visible date range.
+        /// </summary>
+        private ScheduleAppointmentCollection GenerateRandomAppointments()
+        {
+            var WorkWeekDays = new ObservableCollection<DateTime>();
+            var WorkWeekSubjects = new ObservableCollection<string>()
+                                                           { "GoToMeeting", "Business Meeting", "Conference", "Project Status Discussion",
+                                                             "Auditing", "Client Meeting", "Generate Report", "Target Meeting", "General Meeting" };
+
+            var NonWorkingDays = new ObservableCollection<DateTime>();
+            var NonWorkingSubjects = new ObservableCollection<string>()
+                                                           { "Go to party", "Order Pizza", "Buy Gift",
+                                                             "Vacation" };
+            var YearlyOccurrance = new ObservableCollection<DateTime>();
+            var YearlyOccurranceSubjects = new ObservableCollection<string>() { "Wedding Anniversary", "Sam's Birthday", "Jenny's Birthday" };
+            var MonthlyOccurrance = new ObservableCollection<DateTime>();
+            var MonthlyOccurranceSubjects = new ObservableCollection<string>() { "Pay House Rent", "Car Service", "Medical Check Up" };
+            var WeekEndOccurrance = new ObservableCollection<DateTime>();
+            var WeekEndOccurranceSubjects = new ObservableCollection<string>() { "FootBall Match", "TV Show" };
+
+            var brush = new ObservableCollection<SolidColorBrush>();
+
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 133, 81, 242)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 140, 245, 219)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 83, 99, 250)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 255, 222, 133)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 45, 153, 255)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 253, 183, 165)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 198, 237, 115)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 253, 185, 222)));
+            brush.Add(new SolidColorBrush(Color.FromArgb(255, 83, 99, 250)));
+
+            Random ran = new Random();
+            DateTime today = DateTime.Now;
+            if (today.Month == 12)
+            {
+                today = today.AddMonths(-1);
+            }
+            else if (today.Month == 1)
+            {
+                today = today.AddMonths(1);
+            }
+
+            DateTime startMonth = new DateTime(today.Year, today.Month - 1, 1, 0, 0, 0);
+            DateTime endMonth = new DateTime(today.Year, today.Month + 1, 30, 0, 0, 0);
+            DateTime dt = new DateTime(today.Year, today.Month, 15, 0, 0, 0);
+            int day = (int)startMonth.DayOfWeek;
+            DateTime CurrentStart = startMonth.AddDays(-day);
+
+            var appointments = new ScheduleAppointmentCollection();
+            for (int i = 0; i < 90; i++)
+            {
+                if (i % 7 == 0 || i % 7 == 6)
+                {
+                    //add weekend appointments
+                    NonWorkingDays.Add(CurrentStart.AddDays(i));
+                }
+                else
+                {
+                    //Add Workweek appointment
+                    WorkWeekDays.Add(CurrentStart.AddDays(i));
+                }
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                DateTime date = WorkWeekDays[ran.Next(0, WorkWeekDays.Count)].AddHours(ran.Next(9, 17));
+
+                var appointment = new ScheduleAppointment();
+
+                appointment.StartTime = date;
+                appointment.EndTime = date.AddHours(1);
+                appointment.AppointmentBackground = brush[i % brush.Count];
+                appointment.Foreground = GetAppointmentForeground(appointment.AppointmentBackground);
+                appointment.Subject = WorkWeekSubjects[i % WorkWeekSubjects.Count];
+
+                appointments.Add(appointment);
+            }
+            int j = 0;
+            int k = 0;
+            int l = 0;
+
+            while (j < YearlyOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)];
+                var appointment = new ScheduleAppointment();
+
+                appointment.StartTime = date;
+                appointment.EndTime = date.AddHours(1);
+                appointment.AppointmentBackground = brush[1];
+                appointment.Foreground = GetAppointmentForeground(appointment.AppointmentBackground);
+                appointment.Subject = YearlyOccurranceSubjects[j];
+
+                appointments.Add(appointment);
+                j++;
+            }
+            while (k < MonthlyOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)].AddHours(ran.Next(9, 23));
+                var appointment = new ScheduleAppointment();
+
+                appointment.StartTime = date;
+                appointment.EndTime = date.AddHours(1);
+                appointment.AppointmentBackground = brush[k];
+                appointment.Foreground = GetAppointmentForeground(appointment.AppointmentBackground);
+                appointment.Subject = MonthlyOccurranceSubjects[k];
+
+                appointments.Add(appointment);
+                k++;
+            }
+            while (l < WeekEndOccurranceSubjects.Count)
+            {
+                DateTime date = NonWorkingDays[ran.Next(0, NonWorkingDays.Count)].AddHours(ran.Next(0, 23));
+                var appointment = new ScheduleAppointment();
+
+                appointment.StartTime = date;
+                appointment.EndTime = date.AddHours(1);
+                appointment.AppointmentBackground = brush[l];
+                appointment.Foreground = GetAppointmentForeground(appointment.AppointmentBackground);
+                appointment.Subject = WeekEndOccurranceSubjects[l];
+
+                appointments.Add(appointment);
+                l++;
+            }
+
+            return appointments;
+        }
+
+        /// <summary>
+        /// Method to get foreground color based on background.
+        /// </summary>
+        /// <param name="backgroundColor">The background color.</param>
+        /// <returns>The background color.</returns>
+        private Brush GetAppointmentForeground(Brush backgroundColor)
+        {
+            var brush = backgroundColor as SolidColorBrush;
+            if (brush.Color.ToString().Equals("#FF8551F2") || brush.Color.ToString().Equals("#FF5363FA") || brush.Color.ToString().Equals("#FF2D99FF"))
+            {
+                return new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 51, 51, 51));
+            }
+        }
+
+        /// <summary>
+        /// Raises the PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed.</param>
+        /// <param name="oldValue">The old value of the property.</param>
+        protected virtual void RaisePropertyChanged(string propertyName, object oldValue = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
