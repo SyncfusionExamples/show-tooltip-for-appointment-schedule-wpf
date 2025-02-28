75b8cdecd91d3602a5d72e565ad398359455ecdb
GokulSF4339 <gokul.ganesan@syncfusion.com>
2025-02-28 18:53:36 +0530

    Added tooltip customization sample

--- SchedulerWPF/SchedulerWPF/MainWindow.xaml.cs
+++ SchedulerWPF/SchedulerWPF/MainWindow.xaml.cs
@@ -1,21 +1,7 @@
-﻿using Syncfusion.UI.Xaml.Scheduler;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-
-using System.Windows.Input;
-
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace WpfScheduler
+﻿namespace WpfScheduler
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
